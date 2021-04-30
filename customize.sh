f1e084a2f854fafddc6fc122e49c79c8344c35f5
vvenemy <vvenemy@gmail.com>
2021-04-30 12:31:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,6 +7,3 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.245/g' openwrt/package/base-files/files/bin/config_generate
-#2 修改DNS
-sed -i "2i # network config" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i :3i uci set network.lan.dns='192.168.1.245'" openwrt/package/lean/default-settings/files/zzz-default-settings
